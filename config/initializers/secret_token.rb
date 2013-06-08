e8113849ee67d02231fbd06cf9a0a5bbf426824f
arrivu <arrivu@arrivu-Satellite-C675.(none)>
2013-06-08 15:09:31 +0530

    refinerycms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Samplerefinery::Application.config.secret_token = '6f5a07c60d445ec5179136a9b50ac8415beae80442aca06804aa520f2fce7478059db630433404760b76d44cafa073fc4de1e2124f4dcc55238d6108f42bc74e'
