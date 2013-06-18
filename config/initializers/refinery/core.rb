7c5658bf7634c247939acb4386c2baa338b05c4b
RamakrishnanK <ramakrishnan@arrivusystems.com>
2013-06-18 17:27:28 +0530

    company name changes

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -15,7 +15,7 @@ Refinery::Core.configure do |config|
   # config.base_cache_key = :refinery
 
   # Site name
-  # config.site_name = "dfhfgh"
+  config.site_name = "Arrivu Infosystems"
 
   # This activates Google Analytics tracking within your website. If this
   # config is left blank or set to UA-xxxxxx-x then no remote calls to
